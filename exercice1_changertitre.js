83222980a4b2a1daa035dbe07dd253d7121f22f6
Mathis Holler <m.holler@orange.fr>
2025-02-12 11:46:43 +0100

    version 2

--- exercice1_changertitre.js
+++ exercice1_changertitre.js
@@ -5,38 +5,15 @@ const pageTitle = document.querySelector('h1');
 // on recupère l'élément avec l'id newsletter (# est utilisé pour les ids)
 const subscribeButton = document.querySelector('#newsletter'); 
 
+const hasSubscribed = true;
+
+// si on a souscrit, on change le texte du bouton :
 if (hasSubscribed) {
-	subscribeButton.innerText = 'Unsubscribe';
-}
-
-const tasks = [
-	"Finir d'écrire la fiche",
-	"Corriger les fautes",
-	"Demander à une encadrante de relire",
-	"Faire tester à des apprenantes"
-];
-
-// on recupère notre liste de tâches (<ul id="tasks"></ul>) :
-const listEl = document.querySelector('ul#tasks');
-listEl.innerHTML = ''; // on vide la liste
-for (const task of tasks) {
-	// on ajoute la tache sous forme d'élément li :
-	listEl.innerHTML += `<li>${task}</li>`;
+	pageTitle.innerText = 'Mes recette de tarte au citron 🍋';
 }
 
 
-
-
-let hasSubscribed = false;
-
-// on écoute l'évènement clique sur le bouton subscribe
-subscribeButton.addEventListener('click', () => {
-	// on inverse la valeur de la variable
-	hasSubscribed = !hasSubscribed;
-	// on change la valeur du texte
-	if (hasSubscribed) {
-		subscribeButton.innerText = 'Unsubscribe';
-	} else {
-		subscribeButton.innerText = 'Subscribe';
-	}
-});
+function hello () {
+    alert('HELOOOO!!');
+}
+subscribeButton.addEventListener('click' , hello); 
